af280d47b0095cc2f0d1fa1cafa6d323c5a2a217
Michael Voitov <voitovmichael@yandex.ru>
2021-02-27 12:30:12 +0300

    fix: Исправить ошбики согласно общим замечаниям

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,6 @@
+// присвоим перемменым элементы формы: 
+//popup, кнопку редактирования профиля, кнопку закрытия формы, саму форму,
+// имя профиля, описание профиля, input для вода имени, input для ввода описания
 let popup = document.querySelector('.popup');
 
 let profileEdit = document.querySelector('.profile__edit');
@@ -8,23 +11,29 @@ let profileDescription = document.querySelector('.profile__description');
 let inputName = document.querySelector('.popup__input_purpose_name');
 let inputDescription = document.querySelector('.popup__input_purpose_description');
 
-profileEdit.addEventListener('click', function () {
+//метод открытия popup-a
+function openPopup () {
   popup.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
-});
+}
 
-popupCloseButton.addEventListener('click', closePopup);
-
-popupSaveButton.addEventListener('submit', function (evt) {
+//метод для обработки отправки формы
+function saveForm(evt) {
   evt.preventDefault();
-  profileName.textContent = "";
-  profileDescription.textContent = "";
-  profileName.insertAdjacentText('afterbegin', inputName.value);
-  profileDescription.insertAdjacentText('afterbegin', inputDescription.value);
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
   closePopup();
-});
+}
 
+//метод закрытия формы
 function closePopup () {
   popup.classList.remove('popup_opened');
 }
+
+// вешаем обработчик события на нажатие кнопки редактирования профиля
+profileEdit.addEventListener('click', openPopup);
+// вешаем обработчик события на отправку формы
+popupSaveButton.addEventListener('submit', saveForm);
+// вешаем обработчик события на закрытие формы
+popupCloseButton.addEventListener('click', closePopup);
